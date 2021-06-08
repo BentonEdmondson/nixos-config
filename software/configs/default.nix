78e86e1db3241b17cea462db1406806623bad6fe
benton <bentonedmondson@gmail.com>
2021-06-08 18:29:35 -0500

    git configuration added

--- software/configs/default.nix
+++ software/configs/default.nix
@@ -1,9 +1,17 @@
 { flakes, ... }: {
+    # don't forget to set enable = true on the options!!
+
     vscode = {
         enable = true;
         package = flakes.nixpkgs.vscodium;
         extensions = [
             flakes.nixpkgs.vscode-extensions.bbenoist.Nix
         ];
+    };
+
+    git = {
+        enable = true;
+        userEmail = "bentonedmondson@gmail.com";
+        userName = "benton";
     };
 }
