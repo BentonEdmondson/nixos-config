1618463d4a7dc6f18b7f206bf2863452e15d4582
benton <bentonedmondson@gmail.com>
2021-09-25 15:22:19 -0500

    pushing some changes over to the blade

--- system/gnome/default.nix
+++ system/gnome/default.nix
@@ -1,9 +1,10 @@
-{ ... }: {
+{ flakes, ... }: {
     # activate gnome (nixos.wiki/wiki/GNOME)
     services.xserver.enable = true;
     services.xserver.displayManager.gdm.enable = true;
     services.xserver.desktopManager.gnome.enable = true;
 
-    # 12hr instead of 24hr clock
-    #services.xserver.displayManager.lightdm.greeters.gtk.clock-format = "%e %b %Y, %I:%M %p";
+    # THEME!
+    environment.systemPackages = [ flakes.nixpkgs.orchis ];
+    environment.etc."xdg/gtk-3.0".text = "gtk-theme-name = \"Orchis\"";
 }
--- system/xdg/default.nix
+++ system/xdg/default.nix
@@ -1,20 +1,34 @@
-{ flakes, ... }: {
+{ flakes, config, ... }: {
     home-manager.users.benton.xdg = {
         enable = true;
         userDirs = {
             enable = true;
             createDirectories = true;
-
+    
             download = "\$HOME/tmp";
             documents = "\$HOME/docs";
-
+    
             music = "\$HOME/media/music";
             pictures = "\$HOME/media/images";
             videos = "\$HOME/media/videos";
-
+    
             desktop = "\$HOME/etal/desktop";
             templates = "\$HOME/etal/templates";
             publicShare = "\$HOME/etal/public";
         };
+    };
+    
+    environment.variables = {
+        # some programs will do defaults so I'll just tell all to do defaults
+        XDG_CONFIG_HOME = "\$HOME/.config";
+        XDG_CACHE_HOME = "\$HOME/.cache";
+        XDG_DATA_HOME = "\$HOME/.local/share";
+        XDG_STATE_HOME = "\$HOME/.local/state";
+
+        # some programs need to be scolded
+        # see https://wiki.archlinux.org/title/XDG_Base_Directory
+        #GNUPGHOME = "\$HOME/.config/gnupg";
+        #HISTFILE = "\$HOME/.local/share/bash/history";
+        #VSCODE_PORTABLE = "\$HOME/.local/share/vscode";
     };
 }
