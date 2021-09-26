45cae85a2a4ff43cfcf8631326e1d54d427c461f
benton <bentonedmondson@gmail.com>
2021-09-25 22:01:13 -0400

    cleaned up common config file

--- common/default.nix
+++ common/default.nix
@@ -8,32 +8,39 @@
         flakes.home
     ];
 
-    #home-manager.sharedModules = [ { home.packages = flakes.lib.mkForce []; } ];
-    #home-manager.users.benton.home.packages = flakes.lib.mkForce [];
-    home-manager.useUserPackages = true;
-    # for purity
-    home-manager.useGlobalPkgs = true;
+    home-manager = {
+        # for purity
+        useGlobalPkgs = true;
+        useUserPackages = true;
+    };
 
-    # enable flakes
-    nix.package = flakes.nixpkgs.nixUnstable;
-    nix.extraOptions = "experimental-features = nix-command flakes";
+    nix = {
+        # enable flakes
+        package = flakes.nixpkgs.nixUnstable;
+        extraOptions = "experimental-features = nix-command flakes";
 
-    # 3:15 am daily is my understanding
-    nix.optimise.automatic = true;
-    nix.gc.automatic = true;
+        optimise.automatic = true;
+        gc.automatic = true;
+    };
 
-    time.timeZone = "America/Chicago";
+    time.timeZone = "America/Detroit";
     sound.enable = true;
     hardware.pulseaudio.enable = true;
     services.xserver.libinput.enable = true;
 
-    users.mutableUsers = false;
-    users.users.benton = {
-        isNormalUser = true;
-        extraGroups = [ "wheel" "networkmanager" ];
-        hashedPassword = "$6$OdSiSlN9o$xHOCBTBgll28xbHfsi0kcC4J3fNngmEYSVzfP57HaHzcO/HKzD0ca8uQLH8rV7PwBSxWEWRlU6sMaRI9Vd/qL1";
+    users = {
+        mutableUsers = false;
+        users = {
+            benton = {
+                isNormalUser = true;
+                extraGroups = [ "wheel" "networkmanager" ];
+                hashedPassword = "$6$OdSiSlN9o$xHOCBTBgll28xbHfsi0kcC4J3fNngmEYSVzfP57HaHzcO/HKzD0ca8uQLH8rV7PwBSxWEWRlU6sMaRI9Vd/qL1";
+            };
+            # disable root login
+            root.hashedPassword = "!";
+        };
     };
-    users.users.root.hashedPassword = "!";
+    # convenience
     security.sudo.wheelNeedsPassword = false;
 
     system.stateVersion = "20.09";
