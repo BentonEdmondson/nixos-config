ef0384fd6f696d9282215d76a218d8673a5e656f
benton <bentonedmondson@gmail.com>
2021-09-26 20:26:10 -0400

    lightdm mini greeter looks great!

--- common/display/default.nix
+++ common/display/default.nix
@@ -9,14 +9,37 @@
                 extraConfig = ''
                     [greeter]
                     show-password-label = false
-                    invalid-password-text = it's called we do a little trolling
-                    password-alignment = center
-                    password-input-width = 12
+                    show-input-cursor = false
+                    password-input-width = 15
+                    invalid-password-text = fuck you!
+
                     [greeter-theme]
+                    background-image = ""
+                    background-color = "#2e3440"
+
+                    password-character = 0
+                    password-background-color = "#2e3440"
+                    password-border-color = "#3b4252"
+                    password-border-width = 8px
+                    layout-space = 15
+                    password-border-radius = 0
+                    
+                    window-color = "#434c5e"
+                    border-width = 0
+                    
+                    # error stuff
                     font = Source Code Pro
+                    font-size = 35px
+                    font-weight = normal
+                    error-color = "#bf616a"
                 '';
             };
         };
         windowManager.qtile.enable = true;
+    };
+
+    home-manager.users.benton.home.file.".config/qtile/config.py" = {
+        source = ./qtile-config.py;
+        onChange = "qtile cmd-obj -o cmd -f restart";
     };
 }
