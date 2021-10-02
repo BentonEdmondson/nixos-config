40bca666e2a75ab6add4796df15c48f77d102072
benton <bentonedmondson@gmail.com>
2021-10-02 16:42:39 -0400

    switched to sway--working nicely!

--- common/display/default.nix
+++ common/display/default.nix
@@ -2,52 +2,21 @@
     services.xserver = {
         enable = true;
         displayManager = {
-            defaultSession = "none+qtile";
+            defaultSession = "sway";
             lightdm.greeters.mini = {
                 enable = true;
                 user = "benton";
-                extraConfig = ''
-                    [greeter]
-                    show-password-label = false
-                    show-input-cursor = false
-                    password-input-width = 15
-                    invalid-password-text = fuck you!
-
-                    [greeter-theme]
-                    background-image = ""
-                    background-color = "#2e3440"
-
-                    password-character = 0
-                    password-background-color = "#2e3440"
-                    password-border-color = "#3b4252"
-                    password-border-width = 8px
-                    layout-space = 15
-                    password-border-radius = 0
-                    
-                    window-color = "#434c5e"
-                    border-width = 0
-                    
-                    # error stuff
-                    font = Source Code Pro
-                    font-size = 35px
-                    font-weight = normal
-                    error-color = "#bf616a"
-                '';
+                extraConfig = builtins.readFile ./lightdm-mini.conf;
             };
         };
-        windowManager.qtile.enable = true;
-    };
-    home-manager.users.benton.xsession = {
-        enable = true;
-        pointerCursor = {
-            package = flakes.nixpkgs.bibata-cursors;
-            name = "Bibata_Classic";
-            size = 48;
-        };
     };
 
-    home-manager.users.benton.home.file.".config/qtile/config.py" = {
-        source = ./qtile-config.py;
-        onChange = "qtile cmd-obj -o cmd -f restart";
+    programs.sway = {
+        enable = true;
     };
+    home-manager.users.benton.home.file.".config/sway/config" = {
+        source = ./sway-config.in;
+        onChange = "sway reload";
+    };
+
 }
--- programs/default.nix
+++ programs/default.nix
@@ -2,13 +2,14 @@
     imports = [ ./configs.nix ];
 
     users.users.benton.packages = [
-        flakes.nixpkgs.firefox
+        flakes.nixpkgs.firefox-wayland
         flakes.nixpkgs.blender
         flakes.nixpkgs.libreoffice
         flakes.nixpkgs.bitwarden
         flakes.nixpkgs.libsForQt5.kdenlive
         flakes.nixpkgs.element-desktop
         flakes.nixpkgs.gparted
+        flakes.nixpkgs.xorg.xeyes
 
         flakes.nixpkgs.zathura
         flakes.knock
