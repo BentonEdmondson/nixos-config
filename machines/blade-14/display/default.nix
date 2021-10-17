cc6370850528178a1308f53502ff0218c1a6c1ad
benton <bentonedmondson@gmail.com>
2021-10-17 13:41:31 -0400

    firefox uses correct cursor now

--- machines/blade-14/display/default.nix
+++ machines/blade-14/display/default.nix
@@ -1,21 +1,31 @@
 { flakes, ... }: {
-    programs.sway.enable = true;
+    programs.sway = {
+        enable = true;
+        extraPackages = [
+            flakes.nixpkgs.dmenu
+            # for the gsettings updater script
+            flakes.nixpkgs.glib
+        ];
+    };
     environment.etc = {
         "sway/config".source = ./sway.conf;
-        "profile".text = ''
-            if [ -z $DISPLAY ] && [ "$(tty)" = "/dev/tty1" ]; then
-                exec sway
-            fi
-        '';
+        # TODO: I don't think this works--it has to be run manually
+        "sway/import-gsettings".source = ./import-gsettings;
     };
     home-manager.users.benton.home.file.".config/sway/wallpaper".source = ./wallpaper;
 
-    home-manager.users.benton.xsession = {
-        enable = true;
-        pointerCursor = {
-            package = flakes.nixpkgs.bibata-cursors;
-            name = "Bibata_Classic";
-            size = 48;
+    home-manager.users.benton = {
+        xsession = {
+            enable = true;
+            pointerCursor = {
+                package = flakes.nixpkgs.bibata-cursors;
+                name = "Bibata_Classic";
+                size = 48;
+            };
+        };
+        gtk = {
+            enable = true;
+            gtk3.extraConfig.gtk-cursor-theme-name = "Bibata_Classic";
         };
     };
 }
